b15ab30d6056d6131653276a1fa9d2c3a7656368
Michał Puchyr <michalpuchyr3@gmail.com>
2026-04-23 19:43:48 +0200

    chore: init repository

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  outDir: './output',
+  build: {
+    inlineStylesheets: 'always',
+  }
+});
