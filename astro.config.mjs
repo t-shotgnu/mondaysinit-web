c9e1b4178b29da7b1e85321436fb28da2ffa0ab4
Michał Puchyr <michalpuchyr3@gmail.com>
2026-04-26 15:04:32 +0200

    chore: remove stylesheet embedding in build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  outDir: './output',
-  build: {
-    inlineStylesheets: 'always',
-  }
+  outDir: "./output",
 });
